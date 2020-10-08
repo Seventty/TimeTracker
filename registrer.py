f98a3b045c1f27b6e52dafcdce2853ebf9a445f6
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-10-08 08:23:21 -0400

    uploading changes

---
+++ registrer.py
@@ -0,0 +1,12 @@
+import datetime
+import json
+from dateutil import parser
+
+class Activity:
+    def alert():
+        """
+        First class
+        """
+        return "working"
+
+print(Activity.alert())
