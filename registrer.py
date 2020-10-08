20b3e228a4608b5fb745c96bc9bea3e3e3eb01c2
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-10-08 08:52:52 -0400

    time entry added

--- registrer.py
+++ registrer.py
@@ -3,10 +3,18 @@ import json
 from dateutil import parser
 
 class Activity:
-    def alert():
-        """
-        First class
-        """
-        return "working"
+    """ This class will make the activity instance obj"""
+    def __init__(self, name, time):
+        self.name = name # This constructor gon' catch the name of the window already open
+        self.time = time #This constructor gon' catch the time catched
 
-print(Activity.alert())
+class TimeEntry:
+    """ This class will make the time entry instance obj"""
+    def __init__(self, start_time, end_time, days, hours, minutes, seconds):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.total_time = start_time - end_time
+        self.days = days
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
