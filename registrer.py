f2285d8c2bf2e4e96774531d44fa4de43457e133
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-10-08 21:04:55 -0400

    classes fixed

--- registrer.py
+++ registrer.py
@@ -3,13 +3,27 @@ import json
 from dateutil import parser
 
 class Activity:
-    """ This class will make the activity instance obj"""
-    def __init__(self, name, time):
-        self.name = name # This constructor gon' catch the name of the window already open
-        self.time = time #This constructor gon' catch the time catched
+    """ Activity name/time driver """
+    def __init__(self, name, time_entries):
+        self.name = name
+        self.time_entries = time_entries
 
-class TimeEntry:
-    """ This class will make the time entry instance obj"""
+    def serialize(self):
+        return {
+            'name':self.name,
+            'time_entries':self.make_time_entires_to_json()
+        }
+
+    def make_time_entires_to_json(self):
+        time_list = []
+        for time in self.time_entries:
+            time_list.append(time.serialize())
+        return time_list
+    
+
+class TimeEntry():
+    """Time format driver"""
+    """ Time constructor"""
     def __init__(self, start_time, end_time, days, hours, minutes, seconds):
         self.start_time = start_time
         self.end_time = end_time
@@ -18,15 +32,21 @@ class TimeEntry:
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
+    
+    """ Time formater thx stackoverflow"""
+    def get_specific_times(self):
+        self.days, self.seconds = self.total_time.days, self.total_time.seconds
+        self.hours = self.days * 24 + self.seconds // 3600
+        self.minutes = (self.seconds % 3600) // 60
+        self.seconds = self.seconds % 60
 
+    """ Serialize whole the time tracked"""
     def serialize(self):
-        """ Formating road serialize the obj """
         return {
-            'start_time': self.start_time.strftime("%Y-%m-%d %H:%M:%S"),
-            'end_time': self.end_time.strftime("%Y-%m-%d %H:%M:%S"),
-            'days': self.days,
-            'hours': self.hours,
-            'minutes': self.minutes,
-            'seconds': self.seconds
+            'start_time':self.start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            'end_time' : self.end_time.strftime("%Y-%m-%d %H:%M:%S"),
+            'days' : self.days,
+            'hours' : self.hours,
+            'minutes' : self.minutes,
+            'seconds' : self.seconds
         }
-
